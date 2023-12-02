06ec81b0d429ecbb26474831bc88b38ead3eb382
newcodecareer <815773677@qq.com>
2023-12-02 22:47:20 +1000

    global primary color theme config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import './theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router}/>);
