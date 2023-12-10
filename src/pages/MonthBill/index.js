d6f041befb413ac9b15b6aeae1b37cb70d18b678
newcodecareer <815773677@qq.com>
2023-12-10 19:32:31 +1000

    group date by month

--- src/pages/MonthBill/index.js
+++ src/pages/MonthBill/index.js
@@ -1,19 +1,27 @@
 import { NavBar, DatePicker } from "antd-mobile";
 import "./index.scss";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import classNames from "classnames";
 import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import _ from "lodash";
 
 const Month = () => {
+  const billList = useSelector((state) => state.bill.billList);
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
+  }, [billList]);
+  // console.log(monthGroup);
+
   const [dateVisible, setDateVisible] = useState(false);
   const [selectedDate, setSelectedDate] = useState(() => {
-    return dayjs(new Date()).format('YYYY | MM') ;
+    return dayjs(new Date()).format("YYYY-MM");
   });
 
   const onConfirm = (date) => {
     setDateVisible(false);
     // console.log(date);
-    const formatDate = dayjs(date).format('YYYY | MM')
+    const formatDate = dayjs(date).format("YYYY-MM");
     setSelectedDate(formatDate);
   };
 
