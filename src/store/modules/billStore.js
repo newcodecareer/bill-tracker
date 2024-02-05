e0d0f0194ec49e1a10f522e2afd156bfdad5fe42
newcodecareer <815773677@qq.com>
2024-02-05 20:20:04 +1000

    feature: add new bill

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -11,10 +11,13 @@ const billStore = createSlice({
     setBillList(state, action) {
       state.billList = action.payload;
     },
+    addBillList(state,action) {
+      state.billList.push(action.payload)
+    }
   },
 });
 
-const { setBillList } = billStore.actions;
+const { setBillList,addBillList } = billStore.actions;
 
 const getBills = () => {
   return async (dispatch) => {
@@ -22,6 +25,12 @@ const getBills = () => {
     dispatch(setBillList(res.data));
   };
 };
+const addBills = (data) => {
+  return async (dispatch) => {
+    const res = await axios.post("http://localhost:8888/ka",data);
+    dispatch(addBillList(res.data));
+  };
+};
 
-export { getBills };
+export { getBills, addBills };
 export default billStore.reducer;
